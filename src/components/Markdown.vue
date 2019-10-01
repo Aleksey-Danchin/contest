e71c88c11ee20776d57fc6aa66e88e8b652a44fc
Aleksey <aleksey.danchin@gmail.com>
2019-10-01 20:15:03 +0300

    Init

---
+++ src/components/Markdown.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+	div(v-html='result')
+</template>
+
+<script>
+import hlj from 'highlight.js'
+import marked from 'marked'
+
+export default {
+	props: {
+		source: {
+			type: String,
+			required: true,
+			default: ''
+		}
+	},
+
+	computed: {
+		result () {
+			return marked(this.source || '', {
+				gfm: true,
+				highlight: code => hlj.highlightAuto(code).value
+			})
+		}
+	}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+import BootstrapVue from 'bootstrap-vue'
 import Vue from 'vue'
+
 import App from './App.vue'
+
+import 'highlight.js/styles/github.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
