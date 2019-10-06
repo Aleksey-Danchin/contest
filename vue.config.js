bfd9aaf81befeebad6ada3b6df00f6210395362c
Aleksey <aleksey.danchin@gmail.com>
2019-10-06 20:09:27 +0300

    Add publiPath field to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const MonacoEditorPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = {
+    publicPath: './',
     configureWebpack: {
         plugins: [
             new MonacoEditorPlugin({
